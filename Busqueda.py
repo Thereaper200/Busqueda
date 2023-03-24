45967bc6654bc1c28f85bb467781b6e85458d847
Thereaper200 <113812163+Thereaper200@users.noreply.github.com>
2023-03-23 17:12:21 -0700

    Busqueda python scrip V1.01

--- Busqueda.py
+++ Busqueda.py
@@ -4,13 +4,22 @@ client = MongoClient('localhost', 27017)
 
 db = client.facturas
 
-coleccion = db.facturas20_21
+coleccion1 = db.facturas20_21
+coleccion2 = db.facturas17_19
+
 
 while True:
     serialnumber = input("Escribe el numero de serie del producto(Si quieres salir escribe 'Salida'): ")
 
-    busqueda = coleccion.find({'SerialNumber': serialnumber})
+    busqueda = coleccion1.find({'SerialNumber': serialnumber})
+
     for documento in busqueda:
         print(f"El {documento['Product']} se encuentra en la factura: {documento['factura']}")
+
+    busqueda = coleccion2.find({'SerialNumber': serialnumber})
+
+    for documento in busqueda:
+        print(f"El {documento['Product']} se encuentra en la factura: {documento['factura']}")
+
     if serialnumber in ["Salida"]:
         break
